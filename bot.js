a157c8b22dc564f95983dc9b2f01294e8a8a7137
1AhMaD12 <51121291+1AhMaD12@users.noreply.github.com>
2019-05-28 16:46:00 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("582927196771975218")
+setInterval(function() {
+channel.send(`نبغا نصير مطانييخ`);
+}, 30)
+})
+
+client.login("NTgyOTI2NTQxNjg0NjA0OTI4.XO065w.YhsV-RwjJmMPQ8X9FK8KOH-1A2w");
